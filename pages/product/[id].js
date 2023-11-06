c867fa7da32a2f86cb8f9f6db56b500ebc986444
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-11-06 23:01:19 +0330

    feat: product comments

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,11 +2,11 @@ import React from "react";
 import ProductsDetails from "@/components/templates/Product/ProductDetails";
 import Comments from "@/components/templates/Product/Comments";
 
-function Product({productData}) {
+function Product({productData , comments}) {
   return (
       <>
         <ProductsDetails data={productData}/>
-        <Comments/>
+        <Comments data={comments}/>
       </>
   );
 }
@@ -28,13 +28,19 @@ export async function getStaticPaths(context){
 export async function getStaticProps(context){
   const {params} = context
 
-  const res = await fetch(`http://localhost:4000/menu/${params.id}`)
-  const product = await res.json()
+  const productRes = await fetch(`http://localhost:4000/menu/${params.id}`)
+  const productData = await productRes.json()
+
+  const commentRes = await fetch(`http://localhost:4000/comments`)
+  const comments = await commentRes.json()
+  const productComments = comments.filter(comment => comment.id === +params.id)
 
   return{
     props:{
-      productData : product
-    }
+      productData,
+      comments: productComments
+    },
+    revalidate: 60 * 60 * 12 //second
   }
 }
 export default Product;
