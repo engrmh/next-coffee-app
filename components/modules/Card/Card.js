741225e7daf279c6b4d27ba096ddd5618593eaa5
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-11-06 21:19:16 +0330

    feat: product page

--- components/modules/Card/Card.js
+++ components/modules/Card/Card.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
-function Card({ title, desc, price, img }) {
+function Card({ title, desc, price, img , id }) {
   return (
-    <div class="row align-items-center mb-5">
-      <div class="col-4 col-sm-3">
-        <img class="w-100 rounded-circle mb-3 mb-sm-0" src={img} alt="" />
-        <h5 class="menu-price">${price}</h5>
+    <div className="row align-items-center mb-5">
+      <div className="col-4 col-sm-3">
+        <img className="w-100 rounded-circle mb-3 mb-sm-0" src={img} alt="" />
+        <h5 className="menu-price">${price}</h5>
       </div>
-      <div class="col-8 col-sm-9">
-        <h4>{title}</h4>
-        <p class="m-0">{desc}</p>
+      <div className="col-8 col-sm-9">
+          <Link href={`/product/${id}`}>
+             <h4>{title}</h4>
+             <p className="m-0">{desc}</p>
+          </Link>
       </div>
     </div>
   );
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,7 +1,40 @@
 import React from "react";
+import ProductsDetails from "@/components/templates/Product/ProductDetails";
+import Comments from "@/components/templates/Product/Comments";
 
-function Product() {
-  return <h1>Product Page :))</h1>;
+function Product({productData}) {
+  return (
+      <>
+        <ProductsDetails data={productData}/>
+        <Comments/>
+      </>
+  );
 }
 
+export async function getStaticPaths(context){
+  const {params} = context
+
+  const res = await fetch(`http://localhost:4000/menu`)
+  const products = await res.json()
+
+  const paths = products.map(product => ({params: {id: String(product.id)}}))
+
+  return{
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context){
+  const {params} = context
+
+  const res = await fetch(`http://localhost:4000/menu/${params.id}`)
+  const product = await res.json()
+
+  return{
+    props:{
+      productData : product
+    }
+  }
+}
 export default Product;
